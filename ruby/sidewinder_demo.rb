590d497bdd45a718ee8274e670e9fe0ab5f7147e
Daemian Mack <daemianmack@gmail.com>
2019-10-18 18:01:43 -0400

    Print seed before potential crash

--- ruby/sidewinder_demo.rb
+++ ruby/sidewinder_demo.rb
@@ -3,11 +3,10 @@ require_relative 'algos/sidewinder_a'
 require_relative 'seed'
 
 seed = Seed.set(ENV['SEED'])
+puts "Seed: #{seed}"
 
 grid = Grid.new(4,4)
 SideWinderA.on(grid, ENV['DEBUG'])
 
 puts "Complete."
 puts grid
-
-puts "Seed: #{seed}"

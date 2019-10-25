61c8fe7e13f884e094c2d40062978081bd3c39f2
Daemian Mack <daemianmack@gmail.com>
2019-10-25 10:52:25 -0400

    fixup! Add debug flag to demo wrappers

--- ruby/algos/binary_tree.rb
+++ ruby/algos/binary_tree.rb
@@ -1,5 +1,5 @@
 class BinaryTree
-  def self.on(grid)
+  def self.on(grid, debug)
     grid.each_cell do |cell|
       neighbors = []
       neighbors << cell.north if cell.north
@@ -8,6 +8,12 @@ class BinaryTree
       neighbor = neighbors.sample
 
       cell.link(neighbor) if neighbor
+
+      if debug
+        puts grid.diag_print(cell) 
+        puts
+      end
+
     end
   end
 end
